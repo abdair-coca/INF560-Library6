016f0234c436124a5159cdee4b53063dd43c7d63
HuascarFedor <huascar.fedor@gmail.com>
2026-04-16 16:34:05 -0400

    feat: avance parcial de la  guia 5

---
+++ app/Models/Author.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'nationality',
+        'birth_date',
+        'biography',
+    ];
+
+    protected $casts = [
+        'birth_date' => 'date'
+    ];
+
+    protected $appends = ['full_name'];
+
+    public function getFullNameAttribute(): string 
+    {
+        return "{$this->first_name} {$this->last_name}";
+    }
+}
---
+++ app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'color'
+    ];
+
+    protected static function boot(): void
+    {
+        parent::boot();
+
+        static::creating (function(Category $category) {
+            if (empty($category->slug)) {
+                $category->slug = Str::slug($category->name);
+            }
+        });
+    } 
+}
